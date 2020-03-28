dc9fbac49fd931e2fc7f296e391fd073dcea0f87
Craig Freeman <freema48@gmail.com>
2020-03-28 23:15:02 +0000

    add test setup

---
+++ DataAccess.Tests/EntityFramework/EntityFrameworkDatabase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Tests.EntityFramework
+{
+    public class EntityFrameworkDatabase<T> : IDatabase<T> where T : class
+    {
+        public readonly DataContext dataContext;
+
+        public EntityFrameworkDatabase()
+        {
+            var dbOptions = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            dataContext = new DataContext(dbOptions);
+        }
+
+        public void AddData(IEnumerable<T> data)
+        {
+            var dbSet = dataContext.Set<T>();
+            dbSet.AddRange(data);
+        }
+
+        public void AddData(T data)
+        {
+            var dbSet = dataContext.Set<T>();
+            dbSet.Add(data);
+        }
+
+        public void UpdateData(T data)
+        {
+            var dbSet = dataContext.Set<T>();
+            dbSet.Update(data);
+        }
+
+        public void UpdateData(IEnumerable<T> data)
+        {
+            var dbSet = dataContext.Set<T>();
+            dbSet.UpdateRange(data);
+        }
+
+        public void Commit()
+        {
+            dataContext.SaveChanges();
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return dataContext.Set<T>();
+        }
+    }
+}
